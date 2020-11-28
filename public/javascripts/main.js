6c23e64f236a5314b9d5441c34c739cdf7f16b7f
Serj Panov <bugurshik@yandex.ru>
2020-11-28 23:27:22 +0300

    Новое представление

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,49 +1,97 @@
-//#region Border-radius changes
-const rangeTL = document.getElementById('tlr'),
-  rangeTR = document.getElementById('trr'),
-  rangeBL = document.getElementById('blr'),
-  rangeBR = document.getElementById('brr')
-
-const resultTL = document.getElementById('result-tlr'),
-  resultTR = document.getElementById('result-trr'),
-  resultBL = document.getElementById('result-blr'),
-  resultBR = document.getElementById('result-brr')
-
-const borderRadiusResult = document.getElementById('border-radius-result')
-
 const target = document.getElementById('cube')
 let unit = 'px'
 
-target.addEventListener('click', function () {
-  alert(this.self)
-})
+//Фильтрует текст. Возвращает только цифры
+String.prototype.numericFilter = function () {
+  return +this[0] ? +this.replace(/[^\d]/g, '') : ''
+}
+//Возвращает чисто в указанном диапазоне
+Number.prototype.inRange = function (min, max) {
+  return this < min ? min : this > max ? max : this
+}
 
-rangeTL.addEventListener('input', function () {
-  set.borderTopLeft = this.value
-})
-rangeTR.addEventListener('input', function () {
-  set.borderTopRight = this.value
-})
-rangeBL.addEventListener('input', function () {
-  set.borderBottomLeft = this.value
-})
-rangeBR.addEventListener('input', function () {
-  set.borderBottomRight = this.value
-})
-resultTL.addEventListener('input', function () {
-  set.borderTopLeft = this.value.numericFilter().inRange(0, 100)
-})
-resultTR.addEventListener('input', function () {
-  set.borderTopRight = this.value.numericFilter().inRange(0, 100)
-})
-resultBL.addEventListener('input', function () {
-  set.borderBottomLeft = this.value.numericFilter().inRange(0, 100)
-})
-resultBR.addEventListener('input', function () {
-  set.borderBottomRight = this.value.numericFilter().inRange(0, 100)
+class Input {
+  constructor(firstId, secondId = 0) {
+    if (secondId) {
+      console.log(this)
+      this.range = document.getElementById(firstId)
+      this.text = document.getElementById(secondId)
+      this.parentNode = this.range.parentNode
+      this.addDoubleInputEvent()
+    } else {
+      this.input = document.getElementById(firstId)
+      this.parentNode = this.input.parentNode
+      this.addInputEvent()
+    }
+  }
+  _value = 0
+  set value(newValue) {
+    this._value = newValue
+    border.update()
+  }
+  get value() {
+    return this.parentNode.isOnDisplay ? this._value + unit : ''
+  }
+  //#region [Select]
+  addInputEvent() {
+    let docElement = this
+    docElement.input.addEventListener('input', function () {
+      docElement.value = this.value
+    })
+  }
+  addDoubleInputEvent() {
+    let docElement = this
+    docElement.text.addEventListener('input', function () {
+      docElement.value = this.value.numericFilter().inRange(0, 100)
+      docElement.range.value = this.value
+    })
+    docElement.range.addEventListener('input', function () {
+      docElement.value = this.value
+      docElement.text.value = this.value
+    })
+  }
+  //#endregion
+}
+
+let borderRad = {
+  tl: new Input('input-tlr', 'input-text-tlr'),
+  tr: new Input('input-trr', 'input-text-trr'),
+  bl: new Input('input-blr', 'input-text-blr'),
+  br: new Input('input-brr', 'input-text-brr'),
+  output: document.getElementById('border-radius-result'),
+
+  update() {
+    let newValue = this.value
+    target.style.borderRadius = newValue
+    this.output.innerHTML = 'border-radius ' + newValue + ';'
+  },
+  get value() {
+    return this.tl.value + ' ' + this.tr.value + ' ' + this.bl.value + ' ' + this.br.value
+  },
+}
+
+let border = {
+  width: new Input('I-border-width'),
+  style: new Input('I-border-style'),
+  color: new Input('I-border-color'),
+  output: document.getElementById('border-result'),
+
+  update() {
+    let newValue = this.value
+    target.style.border = newValue
+    this.output.innerHTML = 'border: ' + newValue + ';'
+  },
+  get value() {
+    return this.width.value + ' ' + this.style.value + ' ' + this.color.value
+  },
+}
+Object.defineProperty(borderRad.tl, 'value', {
+  set(newValue) {
+    borderRad.tl._value = newValue
+    borderRad.update()
+  },
 })
 //Binding
-//document.querySelectorAll('.input-range-border-radius').forEach((e) => e.addEventListener('input', changeBorderRadiusAll))
 document.querySelectorAll('.add-elem').forEach((e) => e.addEventListener('click', changeElemVisibility))
 for (node of document.querySelectorAll('.input-text-border-radius')) {
   node.addEventListener('focus', textInputOnFocus)
@@ -66,17 +114,9 @@ HTMLElement.prototype.lastChildBefore = function (stopClass) {
   }
   return lastChild ? lastChild : undefined
 }
-//Фильтрует текст. Возвращает только цифры
-String.prototype.numericFilter = function () {
-  return +this[0] ? +this.replace(/[^\d]/g, '') : ''
-}
-//Возвращает чисто в указанном диапазоне
-Number.prototype.inRange = function (min, max) {
-  return this < min ? min : this > max ? max : this
-}
 
 function toggleClassVisibility(...classElem) {
-  alert(document.querySelectorAll(classElem).forEach((e) => e.classList.toggle('hide')))
+  document.querySelectorAll(classElem).forEach((e) => e.classList.toggle('hide'))
 }
 function changeElemVisibility() {
   let lastChild = this.parentNode.lastChildBefore('hide')
@@ -89,84 +129,7 @@ function textInputBlur() {
   this.value = this.value == 0 ? 0 : this.value
 }
 
-function changeBorderRadiusAll() {
-  if (this.type == 'text') this.value = this.value.numericFilter().inRange(0, 100)
-}
-
 const set = {
-  //border
-  get border() {
-    return `${this._borderWidht}px ${this._borderStyle} ${this._borderColor}`
-  },
-  borderUpdate() {
-    target.style.border = this.border
-    this.borderResultElementId.innerHTML = 'border: ' + this.border
-  },
-  set borderWidht(newValue) {
-    this._borderWidht = newValue
-    this.borderUpdate()
-  },
-  set borderStyle(newValue) {
-    this._borderStyle = newValue
-    this.borderUpdate()
-  },
-  set borderColor(newValue) {
-    this._borderColor = newValue
-    this.borderUpdate()
-  },
-  borderInputElementId: document.getElementById('border-I'),
-  borderResultElementId: document.getElementById('result-border'),
-  _border: '0 0 0',
-  _borderWidht: 0,
-  _borderStyle: 'none',
-  _borderColoe: 'black',
-
-  //border-radius
-  get borderRadius() {
-    return `${this.borderTopLeft} ${this.borderTopRight} ${this.borderBottomRight} ${this.borderBottomLeft}`
-  },
-  borderRadiusUpdate() {
-    target.style.borderRadius = this.borderRadius
-    //alert(this.borderRadius)
-    borderRadiusResult.innerHTML = 'border-radius: ' + this.borderRadius + ';'
-  },
-  get borderTopLeft() {
-    return rangeTL.parentNode.isOnDisplay ? this._borderTopLeft + unit : ''
-  },
-  set borderTopLeft(newValue) {
-    this._borderTopLeft = newValue
-    resultTL.value = newValue
-    this.borderRadiusUpdate()
-  },
-  get borderTopRight() {
-    return rangeTR.parentNode.isOnDisplay ? this._borderTopRight + unit : ''
-  },
-  set borderTopRight(newValue) {
-    this._borderTopRight = newValue
-    resultTR.value = newValue
-    this.borderRadiusUpdate()
-  },
-  get borderBottomRight() {
-    return rangeBR.parentNode.isOnDisplay ? this._borderBottomRight + unit : ''
-  },
-  set borderBottomRight(newValue) {
-    this._borderBottomRight = newValue
-    resultBR.value = newValue
-    this.borderRadiusUpdate()
-  },
-  get borderBottomLeft() {
-    return rangeBL.parentNode.isOnDisplay ? this._borderBottomLeft + unit : ''
-  },
-  set borderBottomLeft(newValue) {
-    this._borderBottomLeft = newValue
-    resultBL.value = newValue
-    this.borderRadiusUpdate()
-  },
-  _borderTopLeft: 0,
-  _borderTopRight: 0,
-  _borderBottomRight: 0,
-  _borderBottomLeft: 0,
-
   //box-shadow
   get boxShadow() {
     return `${this._boxShadowOffsetX}px ${this._boxShadowOffsetY}px ${this._boxShadowBlur}px ${this._boxShadowSpread}px 
@@ -241,14 +204,6 @@ function OnLoaded(evt) {
   }
 }
 
-//открыть & закрыть левое меню(при маленьком размере экрана)
-const navToggle = document.getElementById('label-toggle')
-const nav = document.getElementById('lpanel')
-function toToggleNav() {
-  nav.classList.toggle('open')
-  navToggle.classList.toggle('open')
-}
-
 //Копирование содержимого в буфер
 function copyToBuffer(textContainer) {
   //выделение текста
@@ -265,8 +220,16 @@ function copyToBuffer(textContainer) {
   window.getSelection().removeAllRanges()
 }
 
-Object.defineProperty(String.prototype, 'prop', {
-  get() {
-    return this
-  },
-})
+//#region [Mobile]
+
+//открыть & закрыть левое меню(при маленьком размере экрана)
+const navToggle = document.getElementById('label-toggle')
+const nav = document.getElementById('lpanel')
+function toToggleNav() {
+  nav.classList.toggle('open')
+  navToggle.classList.toggle('open')
+}
+//#endregion
+//#region [Tests]
+
+//#endregion
